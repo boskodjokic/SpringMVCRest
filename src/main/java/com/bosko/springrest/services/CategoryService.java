5b90acc8a886f96119424f7ebd69c0739832156a
boskodjokic <boskodjokic@yahoo.com>
2021-05-07 10:38:41 +0200

    Adding categoryService and categorycontroller

---
+++ src/main/java/com/bosko/springrest/services/CategoryService.java
@@ -0,0 +1,12 @@
+package com.bosko.springrest.services;
+
+import com.bosko.springrest.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
